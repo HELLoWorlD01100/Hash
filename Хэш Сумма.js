e28128cfd34e13c6495a3a5a417ca68a05f8645c
HELLoWorlD01100 <54909098+HELLoWorlD01100@users.noreply.github.com>
2019-12-19 22:39:49 +0500

    Update Хэш Сумма.js

--- Хэш Сумма.js
+++ Хэш Сумма.js
@@ -9,9 +9,8 @@ let string = 'не данил';
 let substring = 'данил';
 var result = [];
 var substringHash = hashSum(substring);
-var stringHash = 0
+var stringHash = hashSum(string.substr(0, substring.length));
 for (var i = 0; i <= string.length - substring.length; i++) {
-  stringHash = hashSum(string.substr(i, substring.length));
   if (substringHash == stringHash){
     for (var j = 0; string.charAt(i + j) === substring.charAt(j); j++) {
         if (j == substring.length - 1) {
@@ -20,7 +19,7 @@ for (var i = 0; i <= string.length - substring.length; i++) {
         }
       }
     }
+  stringHash -= string.charCodeAt(i);
+  stringHash += string.charCodeAt(i + substring.length);
 }
 console.log(result.toString())
-
-
