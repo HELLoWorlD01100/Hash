b83328be783ab33a6dd3d402d593e86cb1b37a51
HELLoWorlD01100 <54909098+HELLoWorlD01100@users.noreply.github.com>
2019-12-21 22:20:59 +0500

    Update Алгоритм Рабина - Карпа.js

--- Алгоритм Рабина - Карпа.js
+++ Алгоритм Рабина - Карпа.js
@@ -1,8 +1,7 @@
-function hashSum(string) 
-{
+function hashSum(string) {
     var hash = 0;
     for (var i = 0; i < string.length; i++)
-        hash += string.charCodeAt(i) * Math.pow(2,(string.length - i));
+        hash += string.charCodeAt(i) * Math.pow(2, (string.length - i));
     return hash;
 }
 let string = 'abcabc';
@@ -11,22 +10,20 @@ let result = '';
 let substringHash = hashSum(substring);
 let stringHash = hashSum(string.substr(i, substring.length));
 for (var i = 0; i <= string.length - substring.length; i++) {
-  if (substringHash == stringHash){
-    for (var j = 0; string[ i + j] == substring.charAt(j); j++) {
-        if (j == substring.length - 1) {
-            result += i;
-            break;
+    if (substringHash == stringHash) {
+        for (var j = 0; string[i + j] == substring.charAt(j); j++) {
+            if (j == substring.length - 1) {
+                result += i;
+                break;
+            }
         }
-      }
     }
-    stringHash = 2 * (stringHash - string.charCodeAt(i) * Math.pow(2, substring.length)
-    + string.charCodeAt(i + substring.length));
+    stringHash = 2 * (stringHash - string.charCodeAt(i) * Math.pow(2, substring.length) +
+        string.charCodeAt(i + substring.length));
 }
-if (result.length != 0){
-  for (let i = 1; i <= result.length; i++){
-  
-  console.log(i + ' вхождение: ' + result[i-1] + ' позиция')}
-}
-else console.log('Вхождений нет!')
+if (result.length != 0) {
+    for (let i = 1; i <= result.length; i++) {
 
-
+        console.log(i + ' вхождение: ' + result[i - 1] + ' позиция')
+    }
+} else console.log('Вхождений нет!')
