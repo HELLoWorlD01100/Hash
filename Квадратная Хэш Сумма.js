33169e2899706d76e06fde992c68b3ffadb11b67
HELLoWorlD01100 <54909098+HELLoWorlD01100@users.noreply.github.com>
2019-12-21 22:20:36 +0500

    Update Квадратная Хэш Сумма.js

--- Квадратная Хэш Сумма.js
+++ Квадратная Хэш Сумма.js
@@ -1,5 +1,4 @@
-function hashSum(string) 
-{
+function hashSum(string) {
     let hash = 0;
     for (var i = 0; i < string.length; i++)
         hash += Math.pow(string.charCodeAt(i), 2);
@@ -11,17 +10,15 @@ let result = [];
 let substringHash = hashSum(substring);
 let stringHash = hashSum(string.substr(0, substring.length));
 for (var i = 0; i <= string.length - substring.length; i++) {
-  if (substringHash == stringHash){
-    for (var j = 0; string.charAt(i + j) == substring.charAt(j); j++) {
-        if (j == substring.length - 1) {
-            result.push(i);
-            break;
+    if (substringHash == stringHash) {
+        for (var j = 0; string.charAt(i + j) == substring.charAt(j); j++) {
+            if (j == substring.length - 1) {
+                result.push(i);
+                break;
+            }
         }
-      }
     }
-	stringHash -= Math.pow(string.charCodeAt(i), 2);
-  stringHash += Math.pow(string.charCodeAt(i + substring.length), 2);
+    stringHash -= Math.pow(string.charCodeAt(i), 2);
+    stringHash += Math.pow(string.charCodeAt(i + substring.length), 2);
 }
 console.log(result.toString())
-
-
